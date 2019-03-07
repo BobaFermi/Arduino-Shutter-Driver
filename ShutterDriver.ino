d0bdef0469b5a2ead6c38500b71dfd2020bec5e3
Craig Colquhoun <48094998+BobaFermi@users.noreply.github.com>
2019-03-07 22:47:46 +0000

    Update and rename ShutterDriver_v03.ino to ShutterDriver.ino

--- ShutterDriver.ino
+++ ShutterDriver.ino
@@ -3,6 +3,8 @@ Timer t;
 
 // use motor shield
 #include <AFMotor.h>
+
+//instantiate four shutter objects
 AF_DCMotor shut1(1);
 AF_DCMotor shut2(2);
 AF_DCMotor shut3(3);
@@ -34,71 +36,78 @@ const long tWait = 100;
 
 
 void setup() {
-
+  //set maximum current for driving shutters
   shut1.setSpeed(255);
   shut2.setSpeed(255);
   shut3.setSpeed(255);
   shut4.setSpeed(255);
 
+  //run forward to open all shutters
   shut1.run(FORWARD);
   shut2.run(FORWARD);
   shut3.run(FORWARD);
   shut4.run(FORWARD);
 
+  //set up all LED pins as outputs
   pinMode(LED_1, OUTPUT); 
   pinMode(LED_2, OUTPUT); 
   pinMode(LED_3, OUTPUT); 
   pinMode(LED_4, OUTPUT); 
 
+  //set up all digital inputs as inputs
   pinMode(INP_1, INPUT);  
   pinMode(INP_2, INPUT);      
   pinMode(INP_3, INPUT);      
   pinMode(INP_4, INPUT);      
 
-  // set initial status 
+  //set initial status to 'closed' status
   status_1 = 1;
   status_2 = 1;
   status_3 = 1;
   status_4 = 1;
 
+  //change to open mode
   changeStatus1(0);
   changeStatus2(0);
   changeStatus3(0);
   changeStatus4(0);
 }
 
-
+//stops driving current through shutter 1
 void resetOutput1() {
   shut1.run(RELEASE);
 }
+//stops driving current through shutter 2
 void resetOutput2() {
   shut2.run(RELEASE);
 }
+//stops driving current through shutter 3
 void resetOutput3() {
   shut3.run(RELEASE);
 }
+//stops driving current through shutter 4
 void resetOutput4() {
   shut4.run(RELEASE);
 }
 
-
+//used to control direction of current in shutter 1
 void changeStatus1(int mode) {
-  if ((mode == 1) && (status_1 != 1)) {
-    shut1.run(FORWARD);
-    status_1 = 1;
-    t.after( tWait, resetOutput1 );
-    digitalWrite(LED_1, HIGH);
-    return;
+  if ((mode == 1) && (status_1 != 1)) {   //if shutter status is closed, and we select open mode
+    shut1.run(FORWARD);                   //drive forward current to open the shutter
+    status_1 = 1;                         //keep track of the open sttus of the shutter
+    t.after( tWait, resetOutput1 );       //after tWait time, stop driving current
+    digitalWrite(LED_1, HIGH);            //switch shutter 1 LED indicator on
+    return;                               //go back to start of void loop()
   }
-  if ((mode == 0) && (status_1 != 0)) {
-    // set status 1
-    shut1.run(BACKWARD);
-    status_1 = 0;
-    t.after( tWait, resetOutput1 );
-    digitalWrite(LED_1, LOW);
+  if ((mode == 0) && (status_1 != 0)) {   //if shutter status is open, and we select closed mode
+    shut1.run(BACKWARD);                  //drive reverse current to close the shutter
+    status_1 = 0;                         //keep track of the shutter closed status
+    t.after( tWait, resetOutput1 );       //after tWait duration, stop driving current
+    digitalWrite(LED_1, LOW);             //switch off shutter 1 LED indicator
   }
 }
 
+//used to control direction of current in shutter 2
 void changeStatus2(int mode) {
   if ((mode == 1) && (status_2 != 1)) {
     shut2.run(FORWARD);
@@ -116,6 +125,7 @@ void changeStatus2(int mode) {
   }
 }
 
+//used to control direction of current in shutter 3
 void changeStatus3(int mode) {
   if ((mode == 1) && (status_3 != 1)) {
     shut3.run(FORWARD);
@@ -132,6 +142,7 @@ void changeStatus3(int mode) {
   }
 }
 
+//used to control direction of current in shutter 4
 void changeStatus4(int mode) {
   if ((mode == 1) && (status_4 != 1)) {
     shut4.run(FORWARD);
@@ -150,14 +161,14 @@ void changeStatus4(int mode) {
 
 void loop() {
 
-  t.update();
+  t.update();       //make sure we haven't gone over tWait time yet
 
-  TTL_1 = digitalRead(INP_1);
+  TTL_1 = digitalRead(INP_1);   //check the digital input to control shutter 1
   
-  if ((TTL_1 == HIGH) && (status_1 == 0) ) {
-    changeStatus1(1);
-  } else if ((TTL_1 == LOW) && (status_1 == 1)) {
-    changeStatus1(0);
+  if ((TTL_1 == HIGH) && (status_1 == 0) ) {        //if the status is closed and the digital input is high
+    changeStatus1(1);                               //drive current to open shutter
+  } else if ((TTL_1 == LOW) && (status_1 == 1)) {   //if the status is open and the digital input is low
+    changeStatus1(0);                               //drive current to close the shutter
   }
 
   TTL_2 = digitalRead(INP_2);
